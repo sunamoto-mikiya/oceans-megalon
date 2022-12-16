ff4a40d6ae156d10c795a6655dbc331a092c1423
ryuichi-maeda <uryu44213@gmail.com>
2022-12-16 14:24:13 +0900

    Task系ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Task
+    Route::prefix('projects/{projectId}')->name('task.')->group(function () {
+        Route::get('/', [TaskController::class, 'index'])->name('index');
+        Route::get('/create', [TaskController::class, 'create'])->name('create');
+        Route::post('/', [TaskController::class, 'store'])->name('store');
+        Route::get('/tasks/{taskId}', [TaskController::class, 'show'])->name('show');
+        Route::get('/tasks/{taskId}/edit', [TaskController::class, 'edit'])->name('edit');
+        Route::post('/tasks/{taskId}', [TaskController::class, 'update'])->name('update');
+        Route::delete('/tasks/{taskId}', [TaskController::class, 'delete'])->name('delete');
+    });
 });
 
 require __DIR__.'/auth.php';
