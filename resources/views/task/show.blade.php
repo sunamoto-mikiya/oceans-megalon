c8e4bfc4c86482a965431b0b990be07e2f530fb8
sunamoto-mikiya <miki38gtr@gmail.com>
2022-12-18 03:57:56 +0900

    課題の詳細ページの作成

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -1,15 +1,55 @@
 @extends('adminlte::page') @section('title', 'Dashboard')
 @section('content_header')
-<h1>課題詳細</h1>
+<div class="card-header border-transparent">
+    <h1 class="card-title">課題詳細</h1>
+    <div class="card-tools">
+        <a
+            href="{{ route('task.edit',['projectId'=>$projectId,'taskId'=>$task->id]) }}"
+            class="btn btn-m btn-secondary float-right m-2"
+            >編集</a
+        >
+    </div>
+    <div class="card-tools">
+        <form
+            class="col s12"
+            method="POST"
+            action="{{ route('task.delete',['projectId'=>$projectId,'taskId'=>$task->id]) }}"
+        >
+            @csrf @method('DELETE')
+            <button
+                type="submit"
+                class="btn btn-m btn-secondary float-right m-2"
+            >
+                削除
+            </button>
+        </form>
+    </div>
+</div>
+
 @stop @section('content')
-<div>
-    <p>{{ $taskOptions[$task->type] }}</p>
-    <p>{{ $task->title }}</p>
-    <p>{{ $task->user?->name }}</p>
-    <p>{{ $task->description }}</p>
-    <p>{{ $statusOptions[$task->status] }}</p>
-    <p>{{ $task->start_date }}</p>
-    <p>{{ $task->end_date }}</p>
+<div class="card card-primary">
+    <div class="card-header">
+        <h3 class="card-title">{{ $task->title }}</h3>
+    </div>
+
+    <div class="card-body">
+        <strong><i class="fas fa-book mr-1"></i> タスクタイプ</strong>
+        <p class="text-muted">
+            {{ $taskOptions[$task->type] }}
+        </p>
+        <hr />
+        <strong><i class="far fa-file-alt mr-1"></i> 詳細</strong>
+        <p class="text-muted">{{ $task->description }}</p>
+        <hr />
+        <strong><i class="fas fa-people-arrows mr-1"></i> 担当者</strong>
+        <p>{{ $task->user?->name }}</p>
+        <hr />
+        <strong><i class="fas fa-pencil-alt mr-1"></i> 進捗</strong>
+        <p>{{ $statusOptions[$task->status] }}</p>
+        <hr />
+        <strong><i class="fas fa-calendar-day mr-1"></i> 開始日〜終了日</strong>
+        <p>{{ $task->start_date }}〜{{ $task->end_date }}</p>
+    </div>
 </div>
 @stop @section('css')
 <link rel="stylesheet" href="/css/admin_custom.css" />
