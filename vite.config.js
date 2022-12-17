228892e1d6608862be881c7be971f93877e501c0
sunamoto-mikiya <miki38gtr@gmail.com>
2022-12-17 14:21:26 +0900

    adminLteの導入

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: true,
-        hmr: {
-            host: 'localhost',
-        },
-    },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
