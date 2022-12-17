b5b5a00114701368a3669c092471cf089c15c368
sunamoto-mikiya <miki38gtr@gmail.com>
2022-12-18 01:21:20 +0900

    ロゴ削除

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -1,12 +1,5 @@
 <aside class="main-sidebar {{ config('adminlte.classes_sidebar', 'sidebar-dark-primary elevation-4') }}">
 
-    {{-- Sidebar brand logo --}}
-    @if(config('adminlte.logo_img_xl'))
-        @include('adminlte::partials.common.brand-logo-xl')
-    @else
-        @include('adminlte::partials.common.brand-logo-xs')
-    @endif
-
     {{-- Sidebar menu --}}
     <div class="sidebar">
         <nav class="pt-2">
